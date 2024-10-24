627f81acbaf6fe742f33f5102eaa875a894b4c14
PhilipeF <philipefdev@gmail.com>
2024-10-23 23:32:32 -0300

    Componente Header ok

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,25 @@
+import { CarrinhoProdutosContainer, HeaderBar, NavContainer } from './styles'
 import logoEplay from '../../assets/logo.svg'
-import HeaderBar from './styles'
+import carrinhoIcon from '../../assets/carrinho.svg'
 
 const Header = () => (
     <HeaderBar>
-        <img src={logoEplay} alt="logo da empresa" />
+        <div>
+            <a href="#">
+                <img src={logoEplay} alt="logo da empresa" />
+            </a>
+            <NavContainer>
+                <ul>
+                    <li>Categorias</li>
+                    <li>Novidades</li>
+                    <li>Promoções</li>
+                </ul>
+            </NavContainer>
+        </div>
+        <CarrinhoProdutosContainer>
+            0 - produtos(s)
+            <img src={carrinhoIcon} alt="iconeCarrinho" />
+        </CarrinhoProdutosContainer>
     </HeaderBar>
 )
 
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,10 +1,39 @@
 import styled from "styled-components";
 
-const HeaderBar = styled.div`
+export const HeaderBar = styled.div`
     background-color: var(--gray);
     padding: 24px;
     border-radius: 16px;
     margin-top: 40px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    div {
+        display: flex;
+        align-items: center;
+    }
+
 `
 
-export default HeaderBar
+export const NavContainer = styled.nav`
+    ul {
+        list-style: none;
+        display: flex;
+        gap: 20px;
+        margin-left: 40px;
+        
+    }
+
+    li {
+        font-weight: 700;
+    }
+`
+
+export const CarrinhoProdutosContainer = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    font-weight: 700;
+
+`
