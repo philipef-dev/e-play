32787d3966e8c0e56e899c899a7bd3b13f4c7483
PhilipeF <philipefdev@gmail.com>
2024-10-24 15:21:57 -0300

    Banner, Titulo e Preço - ok

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,7 +1,12 @@
-import { ImageBanner } from "./styles"
+import { ImageBanner, Precos, Titulo } from "./styles"
 
 const Banner = () => (
-    <ImageBanner />
+    <ImageBanner>
+        <div className="container">
+            <Titulo>Marvel's Spider-Man: Miles <br /> Morales PS4 & PS5 </Titulo>
+            <Precos> <span>De R$ 250,00</span>  <br />por apenas R$ 99,90</Precos>
+        </div>
+    </ImageBanner>
 )
 
 export default Banner
--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -8,4 +8,21 @@ export const ImageBanner = styled.div`
     background-image: url(${bannerImg});
     background-size: cover;
     background-repeat: no-repeat;
+    padding-top: 340px;
+`
+
+export const Titulo = styled.h2`
+    font-size: 36px;
+    max-width: 450px;
+`
+
+export const Precos = styled.p`
+    font-size: 24px;
+    margin-top: 24px;
+    font-weight: bold;
+    line-height: 28px;
+
+    span {
+        text-decoration: line-through;
+    }
 `
