4bf7fdb0f4930f842e7b3234b0ddd188e027bb29
PhilipeF <philipefdev@gmail.com>
2024-10-28 19:38:12 -0300

    Separando os arquivos de rotas em pastas

--- src/components/Tag/styles.ts
+++ src/components/Tag/styles.ts
@@ -7,6 +7,6 @@ export const TagContainer = styled.div<Props>`
     color: var(--white);
     padding: ${({ size }) => (size === 'small' ? '4px 6px' : '8px 16px')};
     border-radius: 8px;
-    font-size: 10px;
+    font-size: ${({ size }) => (size === 'small' ? '10px' : '16px')};
     display: inline-block;
 `
