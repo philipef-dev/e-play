b294c23dcaa119b40be84393ce6c327f472b6afa
PhilipeF <philipefdev@gmail.com>
2025-01-06 12:44:12 -0300

    Atualizando a main

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -35,6 +35,7 @@ const Header = () => {
                     </S.CartButton>
                 </div>
             </S.HeaderRow>
+            {/* É aqui que eu quero chegar */}
             <S.NavMobile className={isMenuOpen ? 'is-open' : ''}>
                 <S.Links>
                     <S.LinksItem>
